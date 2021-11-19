228499747b6e21c9fc8a9bf32549c62d663c3f27
kimkisol <kimkisol@naver.com>
2021-11-20 01:14:30 +0900

    1120 kisol

--- final-pjt-back/server/urls.py
+++ final-pjt-back/server/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts', include('accounts.urls')),
     path('api/v1/baskets', include('baskets.urls')),
-    path('api/v1/movies', include('movies.urls')),
-    path('api/v1/tastingrooms', include('tastingrooms.urls')),
+    path('api/v1/movies/', include('movies.urls')),
+    path('api/v1/tastingrooms/', include('tastingrooms.urls')),
 ]
