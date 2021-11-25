eeecd4c1c43ac642e8c1f36251f9309c3dcc0a5f
jiyoon <jiyoonhan12@gmail.com>
2021-11-25 11:58:56 +0900

    group design

--- final-pjt-front/src/components/Basket/CommentForm.vue
+++ final-pjt-front/src/components/Basket/CommentForm.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <div class="text-start d-flex align-items-center mt-2">
-      <span class="me-2">스포일러를 포함하기</span>
-      <input type="checkbox" name="spoiler" v-model="spoiler"><br>
+      <span class="me-2" style="color: #5a89cf; font-size: 0.8rem;">스포일러를 포함하기</span>
+      <b-form-checkbox v-model="spoiler"></b-form-checkbox>
+      <!-- <input type="checkbox" name="spoiler" style="border-color: #5a89cf;" v-model="spoiler"><br> -->
     </div>
-    <div class="">
+    <div>
       <input type="text" @keypress.enter="createAndErase({ content, spoiler })" v-model.trim="content" class="form-control mt-2 mb-4" placeholder="댓글을 입력해주세요.">
       <!-- <button @click="createAndErase({ content, spoiler })" class="mb-3">등록하기</button> -->
     </div>
